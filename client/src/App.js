f09aa067814dbf9aa177f9e11e763d2ae716445b
vijayreddy703 <gimbleboy662@gmail.com>
2025-07-10 15:04:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           Hello welcome vijay this is a sample project
+           Hello welcome vijay
         </p>
         <a
           className="App-link"
